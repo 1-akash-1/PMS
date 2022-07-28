70d7995c8f401dc2ace135a09a9e7b2a45c088bb
Akash O <akashork312@gmail.com>
2022-07-28 08:33:02 -0800

    minor correction

--- dbconfig.php
+++ dbconfig.php
@@ -1,5 +1,5 @@
 				<?php
 
-				$conn = new mysqli('localhost', 'root', '', 'loginsystem') 
+				$conn = new mysqli('localhost', 'root', '', 'loginsystem.sql') 
 					or die ('Cannot connect to db');
 				?>
