de8357991acb20d9a0b2288c06c2fabd19364e0c
Akash O <akashork312@gmail.com>
2022-07-27 22:39:57 -0700

    appointment booking

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,7 @@
 <?php
 //include auth_session.php file on all user panel pages
 include("auth_session.php");
+require('db.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,27 +10,38 @@ include("auth_session.php");
 	<title>Custom Website</title>
 	<link rel="stylesheet" href="style1.css">
 </head>
-<body>
+<body vlink="#ffb3b3">
 	<div class="wrapper">
 			<nav class="navbar">
 				<ul>
+				<li style="float:left;border-right:none"><strong><?php session_start(); echo $_SESSION['email']; ?></strong></li>
 					<li><a class="active" href="#">Home</a></li>
 					<li><a href="#">About</a></li>
-					<li><a href="#">Services</a></li>
-					<li><a href="#">Contact</a></li>
+					<li><a href="cancelbookingpatient.php">Services</a></li>
+					<li><a href="profile.php">Profile</a></li>
 					<li><a href="feedback.html">Feedback</a></li>
 				</ul>
 			</nav>
 			<div class="center">
-			<h1>Welcome <?php echo $_SESSION['username']; ?></h1>
+			<h1>Welcome <?php
+                            $email = $_SESSION['email'];
+$sql = "SELECT username FROM users where email='$email'";
+$result = $con->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<td><td>" . $row["username"] ."</td></td>";
+}
+}
+?></h1>
 			<h2>Select Your Choice</h2>
 			<div class="buttons">
 				<center>
-			<br><button>Request Appointment</button><br>
+			<br><button><a href="book.php" style="text-decoration: none">Request Appointment</a></button><br>
 		    </center>
             <center>
-			<br><button class="btn2">Check Status</button><br>
-			<br><p><button><a href="logout.php">Logout</a></button></p><br>
+			<br><button class="btn2"><a href="viewpatientappointments.php" style="text-decoration: none">Check Status</a></button><br>
+			<br><p><button><a href="logout.php" style="text-decoration: none">Logout</a></button></p><br>
 		</center>
 		</div>
 		</div>
